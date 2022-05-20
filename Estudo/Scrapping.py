4563a937dec014eecf2ccf415d75afd8d3645ab9
CASTREZE SILVA Filipe <filipe.castrezesilva@altran.com>
2022-05-20 10:29:03 +0100

    change url to SEF

--- Estudo/Scrapping.py
+++ Estudo/Scrapping.py
@@ -1,5 +1,5 @@
 import requests
 import time
 
-r = requests.get("https://twitter.com/home")
+r = requests.get("https://www.sef.pt/pt/mySEF/Pages/default.aspx",auth=("castreze@gmail.com","N@t@lia1991"))
 print(r.status_code)
