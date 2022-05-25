be622a34769cf650a3d2fb1590d160f68581220b
CASTREZE SILVA Filipe <filipe.castrezesilva@altran.com>
2022-05-25 10:11:11 +0100

    Dictionary for post

--- Estudo/Scrapping.py
+++ Estudo/Scrapping.py
@@ -1,12 +1,24 @@
 from re import T
 import requests
 
-url_Login = "https://www.sef.pt/_layouts/15/SEF.WebControls/LoginPage.aspx?ReturnUrl=%2fpt%2fmySEF%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252Fpt%252FmySEF%252FPages%252Fautenticacao%252Despecial%252Easpx%253FauthRedirect%253D%25252fpt%25252fmySEF%25252fPages%25252frenovacao%252Dautomatica%252Easpx&Source=%2Fpt%2FmySEF%2FPages%2Fautenticacao%2Despecial%2Easpx%3FauthRedirect%3D%252fpt%252fmySEF%252fPages%252frenovacao%2Dautomatica%2Easpx"
+url_Login = "https://www.sef.pt/_layouts/15/SEF.WebControls/LoginPage.aspx?ReturnUrl=/_layouts/15/error.aspx&Source=%2fpt%2fmySEF%2fPages%2fautenticacao-especial.aspx%3fauthRedirect%3d%252Fpt%252FmySEF%252FPages%252Frenovacao-automatica.aspx"
 url_Renovacao = "https://www.sef.pt/pt/mySEF/Pages/autenticacao-especial.aspx?authRedirect=%2fpt%2fmySEF%2fPages%2frenovacao-automatica.aspx"
 
-r = requests.get(url_Login,auth=("castreze@gmail.com","N@t@lia1991"))
+email = "castreze@gmail.com"
+password = "N@t@lia1991"
+ar = "L29178K46"
+
+dataRenov = {
+    "txtAuthPanelEmail":email,
+    "txtAuthPanelPassword":password,
+    "txtAuthPanelDocument":ar}
+
+r = requests.get(url_Login,auth=(email,password))
+print("\nResponse code Login: " + str(r.status_code) +"\n")
+
+r = requests.post(url_Renovacao,data=dataRenov)
 header = r.headers
-print("\nResponse code: " + str(r.status_code) +"\n")
+print("Response code Renovação: " + str(r.status_code) +"\n")
 
 for i in header:
-    print(str(i) + " : " + str(header[i]))
+   print(str(i) + " : " + str(header[i]))
